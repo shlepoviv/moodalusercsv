6143a2780997326be083531b8d90701641267933
Slepov Ivan <Slepov-Ivan@mail.ru>
2023-11-06 17:36:03 +0300

    add group

--- main.py
+++ main.py
@@ -27,15 +27,16 @@ def user_from_txt(file_txt) -> list:
     return res
     
 
-def user_to_csv(list_user:list[MoodelUser],file_csv,course:str) -> None:
+def user_to_csv(list_user:list[MoodelUser],file_csv,course:str,group:str) -> None:
     with open(file_csv, 'w', newline='',encoding='utf=8') as csvfile:
         fieldnames = ['username','firstname','lastname','email']
-        writer = csv.DictWriter(csvfile, fieldnames=(fieldnames +['course1']))
+        writer = csv.DictWriter(csvfile, fieldnames=(fieldnames +['course1','group1']))
 
         writer.writeheader()
         for user in list_user:
             row = user.getdict(fieldnames)
             row['course1']=course
+            row['group1']=group
             writer.writerow(row)
 
 
@@ -58,7 +59,8 @@ def replace_existing_user_in_list(l_user:list[MoodelUser],ex_user:dict) -> None:
                 logger.warning('replase user with diff: %s',show_diff(l_user[i],ex_user[l_user[i].email]))
             l_user[i] = ex_user[l_user[i].email]
                 
-course = 'Организационные и психолого-педагогические основы инклюзивного высшего образования'
+course = 'Проектная деятельность в высшей школе'
+group = '38-2023'
 
 data_dir = Path('data')
 input_file = Path(data_dir,'input.txt')
@@ -70,4 +72,4 @@ existing_user_file = Path(data_dir,'existing_user.json')
 list_user = user_from_txt(input_file)
 existing_user = read_existing_user(existing_user_file)
 replace_existing_user_in_list(list_user,existing_user)
-user_to_csv(list_user,output_file,course)
+user_to_csv(list_user,output_file,course,group)
